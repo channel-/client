2cd4ac253424957f3333c97ff83a381b1d1d9d0c
Onigiri- <horipod@gmail.com>
2015-02-20 03:27:10 +0900

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # client
+
+next, add channel.html
+
+## main.html
+
+This page is default page, and it supports login.
+
+## Channel.html
+
+This page will be active page. The timeline will add here.
---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngMessages'
+  ]);
